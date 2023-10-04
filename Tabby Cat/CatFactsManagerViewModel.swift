d224c4ba76482a1908a4d91bfe0cb559e8e12411
Sadia Thasina Samanta <sadiakvs.724@gmail.com>
2023-10-05 01:03:03 +0400

    Added VM

--- Tabby Cat/CatFactsManagerViewModel.swift
+++ Tabby Cat/CatFactsManagerViewModel.swift
@@ -1,26 +1,13 @@
 //
-//  CatFactsManager.swift
+//  CatFactsManagerViewModel.swift
 //  Tabby Cat
 //
 //  Created by Sadia Thasina on 05/10/2023.
 //
 
-import SwiftUI
+import Foundation
 
-//    The ViewModel will:
-//    • Attempt to load data from the API
-//    • Convert it from JSON to data
-//    • Store it into a published property of type CatFact
-//    • Tell our View that the property is ready to display
-
-struct CatFactsManagerViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct CatFactsManagerViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        CatFactsManagerViewModel()
-    }
+class CatFactsManager: ObservableObject {
+    
+    
 }
